6d8e24b9dfeea82f318ee28b0c5752f54e972292
Rahul Tiwari <tiwari.ra@northeastern.edu>
2021-11-10 16:59:58 -0500

    code deploy script

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ sudo apt-get update
 sudo apt-get -y upgrade
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install nodejs
+sudo apt install ruby-full
+sudo apt install wget
+cd /home/ubuntu || exit
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+sudo chmod +x ./install
+sudo ./install auto | sudo tee /tmp/logfile > /dev/null
