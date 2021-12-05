107e8b3609bbbcb5922701c7dec457ebf16fcb82
Rahul Tiwari <tiwari.ra@northeastern.edu>
2021-12-04 22:27:46 -0500

    Minor Fix

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 sudo apt-get update -y
 sudo apt-get -y upgrade
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt install nodejs
+sudo apt install -y nodejs
 sudo apt install ruby-full -y
 sudo apt install wget -y
 sudo apt update -y
@@ -14,3 +14,4 @@ wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 sudo chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent start
+sudo npm install pm2 -g
